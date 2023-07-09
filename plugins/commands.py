91bab291462f252f69898e7b3682095e40f9449d
Naruto643 <137090502+Naruto643@users.noreply.github.com>
2023-07-09 21:03:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,6 +66,7 @@ async def start(client, message):
         m=await message.reply_sticker("CAACAgIAAxkBAAKKAAFkHAda7cEwnGhLkERsPowfgFFD2wACVBYAAtB7QUn8uVjZ80ZWKB4E") 
         await asyncio.sleep(1)
         await m.delete()
+        await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
