c18c20ba32f627841e10aa4268a188a532462d69
Naruto643 <137090502+Naruto643@users.noreply.github.com>
2023-07-10 07:56:00 +0530

    Update engine.py

--- plugins/oggy/engine.py
+++ plugins/oggy/engine.py
@@ -1,7 +1,7 @@
 import openai
 
 async def ai(query):
-    openai.api_key = "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" #Your openai api key
+    openai.api_key = "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" #Your openai api key
     response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=100, n=1, stop=None, temperature=0.9, timeout=5)
     return response.choices[0].text.strip()
      
