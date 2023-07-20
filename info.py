38764e1c59c7c90c20987c9297c2db0b12df15ec
Naruto643 <137090502+Naruto643@users.noreply.github.com>
2023-07-20 16:45:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ from Script import script
 
 #Auto approve 
 #In private group or channel must enable request admin approval 
-CHAT_ID = [int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '-1001977832240').split()]
+CHAT_ID = [int(app_chat_id) if 'id_pattern'.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '-1001977832240').split()]
 TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour request has been approved")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
