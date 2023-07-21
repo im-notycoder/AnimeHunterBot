9945491de93ebae19e59107bed39da7731aa38fa
Naruto643 <137090502+Naruto643@users.noreply.github.com>
2023-07-21 11:09:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,6 +45,8 @@ reqst_channel = environ.get('REQST_CHANNEL_ID')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), True)
+# This is required for the plugins involving the file system.
+TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 
 # Command
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
