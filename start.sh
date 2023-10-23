71f02cd7a18c793b62cc6bd711dbccabf38749ee
im-notycoder <130166544+im-notycoder@users.noreply.github.com>
2023-10-23 08:53:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/im-notycoder/AnimeHunterBot.git /AnimeHunterXBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /AnimeHunterXBot
 fi
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
